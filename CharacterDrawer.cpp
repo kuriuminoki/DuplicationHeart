6593d8f38950e4252d9ddfe9b2a94c65bfdc5ad8
kuriu <mkuriu@outlook.jp>
2023-06-06 00:04:26 +0900

    Add CharacterDrawer

---
+++ CharacterDrawer.cpp
@@ -0,0 +1,6 @@
+#include "CharacterDrawer.h"
+
+
+CharacterDrawer::CharacterDrawer() {
+
+}
---
+++ CharacterDrawer.h
@@ -0,0 +1,19 @@
+#ifndef CHARACTER_DRAWER_H_INCLUDED
+#define CHARACTER_DRAWER_H_INCLUDED
+
+class CharacterDrawer {
+private:
+	// •`‰æ‚·‚é‰æ‘œ
+	int m_handle;
+
+	// •`‰æ‚·‚éÀ•W(‰æ‘œ‚Ì’†S)
+	int m_x, m_y;
+
+	// •`‰æ‚·‚é”{—¦
+	int m_ex;
+
+public:
+	CharacterDrawer();
+};
+
+#endif
