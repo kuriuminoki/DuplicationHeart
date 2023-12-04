0f27332ae529d441804105479ca0521a0b44dbbd
kuriuminoki <mkuriu@outlook.jp>
2023-12-04 22:04:08 +0900

    テキストイベントのスキップ機能追加

--- Text.cpp
+++ Text.cpp
@@ -128,6 +128,8 @@ int Conversation::getTextSize() const {
 
 bool Conversation::play() {
 
+	if (leftClick() == 60) { m_finishFlag = true; return true; }
+
 	// I—¹ˆ—
 	if (m_finishCnt > 0) {
 		m_finishCnt++;
